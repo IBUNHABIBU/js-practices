cc4caa4c464ad75f45687f388e24cbe918b7048c
IBUNHABIBU <ibunhabib@gmail.com>
2020-07-08 15:06:09 +0300

    adding local storage

--- Memory Card/app.js
+++ Memory Card/app.js
@@ -15,20 +15,21 @@ let currentActiveCard = 0;
 // store Dom card
 const cardsEl = [];
 // store cards data
-const cardsData = [
-    {
-        question: "What must a variable begin with?",
-        answer: 'A letter, $ or _'
-    },
-    {
-        question: "What is a variable?",
-        answer: 'Container for a piece of data'
-    },
-    {
-        question: "Example of case sensitive variable",
-        answer: 'This is variable'
-    }
-];
+const cardsData = getCardsData();
+// const cardsData = [
+//     {
+//         question: "What must a variable begin with?",
+//         answer: 'A letter, $ or _'
+//     },
+//     {
+//         question: "What is a variable?",
+//         answer: 'Container for a piece of data'
+//     },
+//     {
+//         question: "Example of case sensitive variable",
+//         answer: 'This is variable'
+//     }
+// ];
 
 // create all cards 
 function createCards(){
@@ -68,3 +69,57 @@ createCards();
 function updateCurrentText(){
     currentEl.innerText = `${currentActiveCard+1}/${cardsEl.length}`
 }
+// Get cards from local storage
+function getCardsData(){
+    const cards = JSON.parse(localStorage.getItem('cards'));
+    return (cards === null) ? [] : cards;
+}
+// add card to local storage
+function setCardsData(cards){
+    localStorage.setItem('cards',JSON.stringify(cards));
+    window.location.reload();
+}
+// Arrow event listeners
+nextBtn.addEventListener('click',()=>{
+    cardsEl[currentActiveCard].className = 'card left';
+    currentActiveCard = currentActiveCard+1;
+    if(currentActiveCard>cardsEl.length-1){
+        currentActiveCard = cardsEl.length-1;
+    }
+    cardsEl[currentActiveCard].className = 'card active';
+    updateCurrentText();
+})
+
+prevBtn.addEventListener('click',()=>{
+    cardsEl[currentActiveCard].className = 'card right';
+    currentActiveCard = currentActiveCard-1;
+    if(currentActiveCard<0){
+        currentActiveCard = 0;
+    }
+    cardsEl[currentActiveCard].className = 'card active';
+    updateCurrentText();
+})
+
+//  Show add container 
+showBtn.addEventListener('click', ()=>{
+    addContainer.classList.add('show');
+})
+//  hide add container 
+hideBtn.addEventListener('click', ()=>{
+    addContainer.classList.remove('show');
+})
+
+// add new card
+addCardBtn.addEventListener('click',()=>{
+    const question = questionEl.value;
+    const answer = answerEl.value;
+    if(question.trim()&&answer.trim()){
+        const newCard = { question, answer }
+        createCard(newCard);
+        questionEl.value = '';
+        answerEl.value = '';
+        addContainer.classList.remove('show');
+        cardsData.push(newCard);
+        setCardsData(cardsData);
+    }
+})
