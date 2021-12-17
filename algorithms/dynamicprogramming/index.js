76d0c90cc9431fb5807862e42ae0bf74adbd9fb0
IBUNHABIBU <ibunhabib@gmail.com>
2021-12-17 16:29:20 +0300

    Add how sum problem

--- algorithms/dynamicprogramming/index.js
+++ algorithms/dynamicprogramming/index.js
@@ -70,17 +70,24 @@ console.log("***************** canSum *************************")
 // The function should return a bolean indicating wheather or not it is possible 
 // canSum(7, [2,4,3,7]) true
 
-const canSumRecursive = (target, numbers ) => {
+const canSum = (target, numbers, memo = {} ) => {
+  if(target in memo ) return memo[target];
   if (target === 0) return true;
   if (target < 0 ) return false;
 
   for (let num of numbers) {
     const remainder = target - num;
-    if(canSumRecursive(remainder, numbers) === true) {
+    if(canSum(remainder, numbers, memo) === true) {
+      memo[target] = true;
       return true;
     }
   }
+  memo[target] = false;
   return false
 }
 
-console.log(canSumRecursive(7, [2,4,3,7]));
+console.log(canSum(7, [2, 3]));
+console.log(canSum(7, [2,4,3,7]));
+console.log(canSum(300, [7, 14]));
+
+console.log("******************** How Sum **********************");
