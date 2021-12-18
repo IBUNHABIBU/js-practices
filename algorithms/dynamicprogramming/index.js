b040f96bbe05b67d689ee854b36d3eacc3977169
IBUNHABIBU <ibunhabib@gmail.com>
2021-12-18 09:17:39 +0300

    Finish the can construct

--- algorithms/dynamicprogramming/index.js
+++ algorithms/dynamicprogramming/index.js
@@ -144,4 +144,28 @@ console.log(bestSum(8, [1, 4, 5, 7, 11]));
 console.log(bestSum(100, [1, 2, 5, 25]));
 
 console.log("")
-console.log("***************** bestSum *************************")
+console.log("***************** canConstruct *************************")
+// write a function canConstruct(target, wordBank) that accepts accepts a target string and array of string
+// The function should return a boolean indicating whether or not the target can be constructed by concatenating 
+// element elements of the wordbank array 
+// u may reuse element as many as u can
+const canConstruct = (target, wordBank) => {
+  if(target === '') {
+    return true
+  }
+
+  for (let word of wordBank) {
+    if(target.indexOf(word) === 0) {
+      const suffix = target.slice(word.length);
+      if(canConstruct(suffix, wordBank) === true){
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+console.log(canConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd']));
+console.log(canConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']));
+console.log(canConstruct('enterapotentpot', ['a','p', 'ent', 'enter', 'ot', 'o', 't']));
+console.log(canConstruct('eleuteliusegdiusgangatasjinjajinja', ['ele', 'telius', 'egdius', 'u', 'gangatas', 'jinja']));
