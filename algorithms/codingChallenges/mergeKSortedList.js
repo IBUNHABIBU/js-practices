7872fd74d223fd457723124a2463b4a1813eeee8
IBUNHABIBU <ibunhabib@gmail.com>
2022-01-05 11:46:40 +0300

    make new folder

---
+++ algorithms/codingChallenges/mergeKSortedList.js
@@ -0,0 +1,13 @@
+const mergeTwoList = (l1, l2) => {
+ let list = new Lis
+ 
+}
+
+mergeSortedList = (list) => {
+ if(list.length === 0) return null;
+ while(list.length > 1) {
+  let l1 = list.shif()
+  let l2 = list.shif()
+  let combined = mergeTwoList(l1, l2)
+ }
+}
