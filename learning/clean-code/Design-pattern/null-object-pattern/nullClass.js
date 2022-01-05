d6afb9e908a4396f84922bb1cb0d609ba381cc87
IBUNHABIBU <ibunhabib@gmail.com>
2022-01-05 13:46:12 +0300

    Creating a nullObject pattern

--- learning/clean-code/Design-pattern/null-object-pattern/nullClass.js
+++ learning/clean-code/Design-pattern/null-object-pattern/nullClass.js
@@ -25,16 +25,19 @@ class NullUser {
 const users = [new User(1, 'Bob'), new User(2, 'John')];
 
 function getUser(id) {
- return users.find(user => user.id === id)
+ const user = users.find(user => user.id === id)
+ if(user == null) {
+  return new NullUser();
+ } else {
+  return user;
+ }
 }
 
 function printUser(id) {
  const user = getUser(id);
-   let name = 'Guest';
-   if (user != null && user.name != null) name = user.name;
-   console.log('Hello  ' + name);
+   console.log('Hello  ' + user.name);
 
-   if(user != null && user.hasAccess != null && user.hasAccess()) {
+   if(user.hasAccess()) {
     console.log('You have access');
    } else {
     console.log('You are not allowed');
