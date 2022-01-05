b935bce944769f571d4b4ba096426e8f47cd9ae6
IBUNHABIBU <ibunhabib@gmail.com>
2022-01-05 15:27:19 +0300

    Builder pattern

---
+++ learning/clean-code/Design-pattern/builder-pattern/index.js
@@ -0,0 +1,15 @@
+class Address {
+ constructor(zip, street) {
+  this.zip = zip;
+  this.street = street;
+ }
+}
+
+class User {
+ constructor(name, age, phone, address) {
+  this.name = name;
+  this.age = age;
+  this.phone = phone;
+  this.Address = address;
+ }
+}
