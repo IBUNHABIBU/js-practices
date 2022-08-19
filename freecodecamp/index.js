e909d37776f2b0ee453158343fc3f251ec4ffcaa
IBUNHABIBU <ibunhabib@gmail.com>
2022-08-19 16:15:31 +0300

    Solve lookup profile

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -295,9 +295,7 @@ function lookUpProfile(name, prop) {
     for (var i = 0; i < contacts.length; i++) {
         if(contacts[i].firstName == name) {
             if(contacts[i].hasOwnProperty(prop)) {
-                return contacts[i][prop];
-            } else {
-                return "No such property";
+                return contacts[i][prop] || "No such property";
             }
         }
     }
