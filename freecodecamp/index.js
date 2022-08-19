d01ec56d841aa49548a837548e9b3bd8a88e5d71
IBUNHABIBU <ibunhabib@gmail.com>
2022-08-19 16:08:35 +0300

    Solve lookup profile

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -264,3 +264,34 @@ function multiplyAll(arr) {
 }
 console.log(multiplyAll([[1,2],[3,4],[5,6,7]]));
 
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["Javascript", "Gaming", "Foxes"]
+    }
+];
+
+function lookUpProfile(name, prop) {
+    return "no such property"
+}
+
