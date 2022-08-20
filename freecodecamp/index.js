b25c9e28c9d4f9409fcff81849c13e179097fa4b
IBUNHABIBU <ibunhabib@gmail.com>
2022-08-20 22:04:01 +0300

    Add random generator

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -312,6 +312,6 @@ var randBtn1and19 = Math.floor(Math.random() * 20);
 function ourRandomRange (ourMin, ourMax) {
     return Math.floor(Math.random() * (ourMax - ourMin + 1)) + ourMin;
 }
-function randomRange(ourMin, ourMax) {
+function randomRange(myMin, myMax) {
     
 }
