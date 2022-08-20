16949e517a308ab7665de69262d177777bf52491
IBUNHABIBU <ibunhabib@gmail.com>
2022-08-20 23:11:18 +0300

    Prevent mutation

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -366,12 +366,13 @@ function freezeObject() {
     const MATH_CONSTANTS = {
         PI: 3.14
     };
+    Object.freeze(MATH_CONSTANTS);
     try{
         MATH_CONSTANTS.PI = 9.97
     } catch(ex) {
         console.log(ex)
     }
-    return MATH_CONSTANTS;
+    return MATH_CONSTANTS.PI;
 }
 
 const PI = freezeObject();
