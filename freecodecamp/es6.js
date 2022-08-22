3daa61bf880c2cf2df7710fe93926727d2e9c134
IBUNHABIBU <ibunhabib@gmail.com>
2022-08-22 23:12:55 +0300

    Destructure array

--- freecodecamp/es6.js
+++ freecodecamp/es6.js
@@ -149,8 +149,8 @@ const stats = {
 }
 
 const half = (function(){
-    return function half(stats){
-        return (stats.max + stats.min) / 2.0
+    return function half({ max, min }){
+        return (max + min) / 2.0
     }
 })()
 console.log(stats);
