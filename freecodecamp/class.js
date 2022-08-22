414c0d1ac6a317b85b50b7684a3f0e41261816b8
IBUNHABIBU <ibunhabib@gmail.com>
2022-08-22 23:46:51 +0300

    Add class objects

--- freecodecamp/class.js
+++ freecodecamp/class.js
@@ -1,6 +1,16 @@
+// constructor function creation
 var SpaceShuttle = function(targetPlanet) {
     this.targetPlanet = targetPlanet;
 }
 
 var zeus = new SpaceShuttle("Jupiter")
 console.log(zeus.targetPlanet)
+
+// using class 
+class Car {
+    constructor(name) {
+        this.name = name;
+    }
+}
+let saloon = new Car("benz")
+console.log(saloon.name)
