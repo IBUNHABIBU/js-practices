05b8c3dac3129e0597b5bf18f8921005e9a9ad90
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-15 13:39:09 +0300

    Add word search problem'

--- algorithms/codingChallenges/word_search.js
+++ algorithms/codingChallenges/word_search.js
@@ -1,3 +1,27 @@
 const wordSearch = (board, word) => {
+    const wordLength = word.length;
+    const boardLength = board.length;
+    const boardWidth = board[0].length;
     
+    for (let i = 0; i < boardLength; i++) {
+        for (let j = 0; j < boardWidth; j++) {
+        if (wordSearchHelper(board, word, i, j, 0)) {
+            return true;
+        }
+        }
+    }
+    return false;
+}
+
+const wordSearchHelper = (board, word, i, j, index) => {
+    if (index === word.length) {
+        return true;
+    }
+    if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] !== word[index]) {
+        return false;
+    }
+    board[i][j] = '*';
+    const found = wordSearchHelper(board, word, i + 1, j, index + 1) || wordSearchHelper(board, word, i - 1, j, index + 1) || wordSearchHelper(board, word, i, j + 1, index + 1) || wordSearchHelper(board, word, i, j - 1, index + 1);
+    board[i][j] = word[index];
+    return found;
 }
